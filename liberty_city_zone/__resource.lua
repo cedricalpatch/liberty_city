13b3e4e95249c68d4c71a31300e103f31da350f9
RPF Studio <groupe-esp-fr@outlook.fr>
2019-04-08 12:03:51 +0200

    Add files via upload

---
+++ liberty_city_zone/__resource.lua
@@ -0,0 +1,16 @@
+client_script 'zone_names.lua'
+
+before_level_meta 'data.meta'
+
+files {
+	'popcycle.dat',
+	'popzonelc.ipl',
+	'popzone.ipl',
+	'zonebind.meta',
+	'data.meta',
+	'popgroups.ymt',
+}
+
+data_file 'POPSCHED_FILE' 'popcycle.dat'
+data_file 'ZONEBIND_FILE' 'zonebind.meta'
+data_file 'FIVEM_LOVES_YOU_341B23A2F0E0F131' 'popgroups.ymt'
